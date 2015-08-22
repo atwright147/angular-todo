d4fa4e45005de99695da37fed93185ecd27f19a1
Andy Wright <atwright147@gmail.com>
2015-08-22 18:40:16 +0100

    add initial model

--- js/app.js
+++ js/app.js
@@ -7,4 +7,10 @@ angular.module('app', []);
 
 angular.module('app', []).controller('TodoController', function($scope) {
 	$scope.title = 'Angular Todo List';
+
+	$scope.todos = [
+		{title: 'Buy Groceries', description: 'Fruit, Cereal, Cola, Pies, etc'},
+		{title: 'Print Photos',  description: 'Get photos printed at Boots'},
+		{title: 'Book Holiday',  description: 'Book the hotel we found in Goa'}
+	];
 });
