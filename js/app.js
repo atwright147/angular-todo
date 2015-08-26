5a9dd528e60742c01ee76eed17171fdc6605ec54
Andy Wright <atwright147@gmail.com>
2015-08-26 23:01:37 +0100

    create todos table and select all rows to populate index page

--- js/app.js
+++ js/app.js
@@ -8,13 +8,44 @@ angular.module('app', ['angular-websql']);
 angular.module('app').controller('TodoController', function($scope, $webSql) {
 	$scope.pageTitle = 'Angular Todo List';
 
-	$scope.todos = [
-		{title: 'Buy Groceries', description: 'Fruit, Cereal, Cola, Pies, etc'},
-		{title: 'Print Photos',  description: 'Get photos printed at Boots'},
-		{title: 'Book Holiday',  description: 'Book the hotel we found in Goa'}
-	];
+	// $scope.todos = [
+	// 	{title: 'Buy Groceries', description: 'Fruit, Cereal, Cola, Pies, etc'},
+	// 	{title: 'Print Photos',  description: 'Get photos printed at Boots'},
+	// 	{title: 'Book Holiday',  description: 'Book the hotel we found in Goa'}
+	// ];
 
 	$scope.db = $webSql.openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
+	$scope.db.createTable('todos', {
+		"id":{
+			"type": "INTEGER",
+			"null": "NOT NULL", // default is "NULL" (if not defined)
+			"primary": true, // primary
+			"auto_increment": true // auto increment
+		},
+		"created_at":{
+			"type": "TIMESTAMP",
+			"null": "NOT NULL",
+			"default": "CURRENT_TIMESTAMP" // default value
+		},
+		"title":{
+			"type": "TEXT",
+			"null": "NOT NULL"
+		},
+		"description": {
+			"type": "TEXT",
+			"null": "NULL"
+		},
+		"complete": {
+			"type": "INTEGER"
+		}
+	});
+
+	$scope.db.selectAll('todos').then(function(results) {
+		$scope.todos = [];
+		for(var i=0; i < results.rows.length; i++) {
+			$scope.todos.push(results.rows.item(i));
+		}
+	});
 
 	$scope.removeItem = function(item) {
 		$scope.todos.splice(item, 1);
